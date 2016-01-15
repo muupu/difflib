c790ef0d0e044a808c2a27b7aa7b486d7a2bae95
muupu <once645@gmail.com>
2016-01-15 21:35:43 +0800

    add SequenceMatcher

---
+++ SequenceMatcher.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef DIFFLIB_SEQUENCEMATCHER_H_
+#define DIFFLIB_SEQUENCEMATCHER_H_
+
+class SequenceMatcher
+{
+public:
+	SequenceMatcher();
+	~SequenceMatcher();
+};
+
+#endif  // DIFFLIB_SEQUENCEMATCHER_H_
